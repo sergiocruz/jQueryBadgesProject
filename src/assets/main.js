bb9d96a698861892f30c8e3ab7d8f14d915ae83a
Sergio Cruz <hello@sergiocruz.me>
2016-08-19 15:38:47 -0400

    One possible solution

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,44 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/185856.json',
+    dataType: 'jsonp',
+    success: function(data) {
+      populateWithCourses(data.courses.completed)
+    }
+  });
+
+  function populateWithCourses(courses) {
+
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+
+      $column = $('<div />', {
+        'class': 'col-sm-4 col-md-3'
+      }).appendTo($badges);
+
+      $div = $('<div />', {
+        'class': 'course'
+      }).appendTo($column);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($div);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($div);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($div);
+
+    });
+
+  }
 
 });
