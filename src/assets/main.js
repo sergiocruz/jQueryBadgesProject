aa45582ee34af3d0630cc25adb5b56963299e261
Sergio Cruz <hello@sergiocruz.me>
2016-09-22 23:03:25 -0400

    Making Ajax request

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,11 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/185856.json',
+    dataType: 'jsonp',
+    success: function(response) {
+      
+    }
+  })
 
 });
