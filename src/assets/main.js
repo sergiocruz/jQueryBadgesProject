e2b786c5bb583e6830338cdf13a38870ef757608
Sergio Cruz <hello@sergiocruz.me>
2016-09-22 23:22:53 -0400

    One item per course

--- src/assets/main.js
+++ src/assets/main.js
@@ -4,8 +4,18 @@ $(function() {
     url: 'https://www.codeschool.com/users/185856.json',
     dataType: 'jsonp',
     success: function(response) {
-      
+      buildDom(response.courses.completed);
     }
-  })
+  });
+
+  function buildDom(courses) {
+    var $badges = $('#badges');
+
+    courses.forEach(function(course) {
+      $badges.append($('<div />', {
+        'class': 'course'
+      }));
+    });
+  }
 
 });
